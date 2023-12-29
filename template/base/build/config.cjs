c3309b34aae1ea4357e72ead0aafe7f9799eb643
Your Name <you@example.com>
2023-12-29 12:24:23 +0800

    [fix] config 模块化问题

--- template/base/build/config.cjs
+++ template/base/build/config.cjs
@@ -1,5 +1,5 @@
-import path from 'path';
-// require('dotenv').config({path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`)});
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`)});
 
 /* ========================= 环境 ========================= */
 const IS_PRODUCT = process.env.NODE_ENV === 'production';
@@ -17,7 +17,7 @@ const CLIENT_PATH = path.resolve(__dirname, '../client');
 const OUTPUT = path.resolve(__dirname, '../dist');
 const PUBLIC_PATH = IS_PRODUCT ? './' : '/';
 const IS_CLIENT = process.env.MODE_ENV === 'client';
-export const IS_SERVER = process.env.MODE_ENV === 'server';
+const IS_SERVER = process.env.MODE_ENV === 'server';
 
 /* ========================= 编译 ========================= */
 const DEVTOOL = IS_DEVELOPMENT ? 'source-map' : 'nosources-source-map';
@@ -25,10 +25,29 @@ const DEVTOOL = IS_DEVELOPMENT ? 'source-map' : 'nosources-source-map';
 const serverResolve = pathname => path.resolve(SERVER_PATH, pathname);
 const clientResolve = pathname => path.resolve(CLIENT_PATH, pathname);
 
-export const conditionBack = (condition, value, value1) => {
+const conditionBack = (condition, value, value1) => {
     let res = {};
     if (value instanceof Array) {
         res = [];
     }
     return condition && value || value1 || res;
+};
+
+module.exports = {
+    IS_PRODUCT,
+    IS_DEVELOPMENT,
+    MODE,
+    SRC_PATH,
+    SERVER_PATH,
+    CLIENT_PATH,
+    ENTRY,
+    OUTPUT,
+    PUBLIC_PATH,
+    DEVTOOL,
+    PORT,
+    serverResolve,
+    clientResolve,
+    IS_CLIENT,
+    IS_SERVER,
+    conditionBack
 };
